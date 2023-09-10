1a2f42ff28da3a50ff2e4de32fa3ead8432a2474
Long <laptoptandt@Longs-Macbookpro.local>
2023-09-10 11:36:30 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+ScrollReveal({
+    distance: '50px',
+    duration: 2000,
+    delay: 400
+});
+ScrollReveal().reveal('.home-title, .content-heading', { delay: 100, origin: 'left' });
+ScrollReveal().reveal('.about-us-img, .new-img', { delay: 100, origin: 'bottom' });
+ScrollReveal().reveal('.about-us-text, .new-text', { delay: 100, origin: 'right' });
+ScrollReveal().reveal('.block', { delay: 100, origin: 'bottom', interval: 200 });
+
+
